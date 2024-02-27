40ff15256723de0fff275b2b6fdf48293cf85ce0
DwiyanYusup <yusuprayaka24@gmail.com>
2024-02-27 10:03:48 +0700

    commit

--- tugas/tugas1/1c.php
+++ tugas/tugas1/1c.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas1c</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas1c</title>
 </head>
+
 <body>
   <style>
     .container {
@@ -27,25 +29,27 @@
       background-color: pink;
     }
   </style>
-</head>
-<body>
-  <div class="container">
-    <!-- Baris pertama -->
-    <div class="baris">
-      <div class="kotak">1</div>
-    </div>
-    <!-- Baris kedua -->
-    <div class="baris">
-      <div class="kotak">2</div>
-      <div class="kotak">2</div>
-    </div>
-    <!-- Baris ketiga -->
-    <div class="baris">
-      <div class="kotak">3</div>
-      <div class="kotak">3</div>
-      <div class="kotak">3</div>
-    </div>
-  </div>
+  </head>
 
-</body>
+  <body>
+    <div class="container">
+      <!-- Baris pertama -->
+      <div class="baris">
+        <div class="kotak">1</div>
+      </div>
+      <!-- Baris kedua -->
+      <div class="baris">
+        <div class="kotak">2</div>
+        <div class="kotak">2</div>
+      </div>
+      <!-- Baris ketiga -->
+      <div class="baris">
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+      </div>
+    </div>
+
+  </body>
+
 </html>
